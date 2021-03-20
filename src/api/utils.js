70b964797d12f6bfd997740ef54504490538dc64
AntoninJuquel <antonin.juquel@outlook.fr>
2021-03-20 11:03:09 +0100

    Added api key

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,6 +1,6 @@
 export function BrowseNeo({ url }) {
     return (
-        fetch(url ?? "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY")
+        fetch(url ?? "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=Cb7ABiryaKAwR1uQk1Y9C4SlgLt33VdV0wlACoRF")
             .then(result => result.json())
             .then(res => res)
             .catch(err => err)
