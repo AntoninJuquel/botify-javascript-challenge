3af4c80b3edd948abc77593502f53f6edcbcfa00
AntoninJuquel <antonin.juquel@outlook.fr>
2021-03-21 00:20:24 +0100

    Added filters using most recent orbiting body

--- src/screens/ChartsScreen.js
+++ src/screens/ChartsScreen.js
@@ -2,11 +2,24 @@ import React, { useEffect, useState } from 'react'
 import { Button, Container } from 'react-bootstrap'
 
 import { BrowseNeo } from "../api";
-import { ChartComponent } from "../components";
-import { average, descending } from '../functions';
+import { ChartComponent, DropdownComponent } from "../components";
+import { average, closestDate, descending } from "../functions";
 
 export default function ChartsScreen() {
     const [data, setData] = useState({ links: { next: null, prev: null, curr: null }, neo: [] })
+
+    const [filters, setFilter] = useState({
+        "Earth": false,
+        "Mars": false,
+        "Merc": false,
+        "Juptr": false
+    })
+
+    const selectFilter = (filter) => {
+        const newFilters = { ...filters }
+        newFilters[filter] = !newFilters[filter]
+        setFilter(newFilters)
+    }
 
     const fetchNeo = (url) => {
         setData({ ...data, neo: [] })
@@ -16,7 +29,20 @@ export default function ChartsScreen() {
     const handleResponse = res => {
         const { near_earth_objects, links } = res
 
-        const neoResult = near_earth_objects.map(neo => [
+        const activesFilters = Object.keys(filters).filter(key => filters[key])
+
+        // Select neo with most recent corresponding orbiting body
+        const filteredNeo = near_earth_objects.filter(neo => {
+            const date = closestDate(neo.close_approach_data.map(neoData => new Date(neoData.close_approach_date)))
+            return activesFilters.includes(neo.close_approach_data[date.index].orbiting_body) || activesFilters.length === 0
+        })
+
+        // Browse pages until you find result
+        if (filteredNeo.length === 0 && links.next) {
+            fetchNeo(links.next)
+            return
+        }
+        const neoResult = filteredNeo.map(neo => [
             neo.name,
             neo.estimated_diameter.kilometers.estimated_diameter_min,
             neo.estimated_diameter.kilometers.estimated_diameter_max,
@@ -29,9 +55,13 @@ export default function ChartsScreen() {
 
     useEffect(() => {
         BrowseNeo({}).then(handleResponse)
-    }, [])
+    }, [filters])
+
     return (
         <Container style={{ height: "100%" }}>
+            <Container style={{ position: 'absolute', right: 0, zIndex: 100 }}>
+                <DropdownComponent title="Orbiting body" itemsDict={filters} placeholder="Type to search..." onSelectItem={selectFilter} />
+            </Container>
             <ChartComponent data={data.neo} />
             <Container style={{ position: 'absolute', bottom: 0 }}>
                 <Button disabled={!data.links.prev} onClick={() => fetchNeo(data.links.prev)}>{"<"}</Button>
