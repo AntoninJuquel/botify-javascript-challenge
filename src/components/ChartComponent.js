89a08630cd912aa1f89c5ec1888c11f287de7161
AntoninJuquel <antonin.juquel@outlook.fr>
2021-03-20 10:59:33 +0100

    Added ChartComponent

---
+++ src/components/ChartComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Chart from 'react-google-charts'
+
+export default function ChartComponent({ data }) {
+    return (
+        <Chart
+            width="100%"
+            height="100%"
+            chartType="BarChart"
+            data={[["Neo Name", "Min estimated diameters", "Max estimated diameters"], ...data]}
+            options={{
+                title: 'Near Earth Objects',
+                hAxis: {
+                    title: 'Estimated Diameters (km)',
+                    minValue: 0,
+                },
+                vAxis: {
+                    title: 'Neo Name',
+                },
+            }}
+        />
+    )
+}
