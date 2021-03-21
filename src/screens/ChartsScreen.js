0d44137d8ae2f6bbd3803918da34688e655e1b3c
AntoninJuquel <antonin.juquel@outlook.fr>
2021-03-21 01:40:39 +0100

    Restyled to have more readable code

--- src/screens/ChartsScreen.js
+++ src/screens/ChartsScreen.js
@@ -63,14 +63,14 @@ export default function ChartsScreen() {
             <Container style={{ position: 'absolute', right: 0, zIndex: 100 }}>
                 <DropdownComponent title="Orbiting body" itemsDict={filters} placeholder="Type to search..." onSelectItem={selectFilter} />
             </Container>
-            <ChartComponent format={data.format} data={data.neo} chartTypesArray={["BarChart", "Table"]} />
-            <Container style={{ position: 'absolute', bottom: 0 }}>
+            <Container style={{ position: 'absolute', bottom: 0, left: 0, zIndex: 100 }}>
                 <Button disabled={!data.links.prev} onClick={() => fetchNeo(data.links.prev)}>{"<"}</Button>
                 <Button disabled={!data.links.next} onClick={() => fetchNeo(data.links.next)}>{">"}</Button>
             </Container>
-            <div style={{ position: 'absolute', bottom: 0, right: 0 }}>
+            <Container style={{ position: 'absolute', bottom: 0, right: 0, zIndex: 100 }}>
                 <CSVLink className="btn btn-primary" data={[data.format, ...data.neo]}>Download me</CSVLink>
-            </div>
+            </Container>
+            <ChartComponent format={data.format} data={data.neo} chartTypesArray={["BarChart", "Table"]} />
         </Container>
     )
 }
